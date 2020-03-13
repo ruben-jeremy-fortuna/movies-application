cb0354cff1bf7e473207b3ec8fd76fe85780cbf3
Jeremy Telhiard <jeremy.telhiard@gmail.com>
2020-03-13 11:22:58 -0500

    Starting movies app

--- src/api.js
+++ src/api.js
@@ -2,5 +2,29 @@ module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
       .then(response => response.json());
+  },
+
+  addMovies: () => {
+    return fetch('/api/movies', {
+      method: 'post',
+      headers: 'Content-Type: application/json'
+    }).then(response => response.json());
+  },
+
+  editMovies: () => {
+    return fetch('/api/movies', {
+      method: 'patch',
+      headers: 'Content-Type: application/json'
+    }).then(response => response.json());
+  },
+
+  deleteMovies: () => {
+    return fetch('/api/movies', {
+      method: 'delete',
+      headers: 'Content-Type: application/json'
+    }).then(response => response.json());
   }
+
 };
+
+
